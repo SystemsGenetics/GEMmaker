126ce9f6dc3ec97c8d6438cd3e35dd22c5aab10c
Stephen Ficklin <spficklin@gmail.com>
2019-10-07 17:23:12 -0700

    Update fastq_merge.sh
    Fixed bug in cleanup when the fastq_merge process gets preempted.

--- bin/fastq_merge.sh
+++ bin/fastq_merge.sh
@@ -6,10 +6,21 @@
 sample_id="$1"
 publish="$2"
 
-files1=`ls *_1.fastq | sort`
+# Cleanup any previously existing sample FASTQ files
+if [ -e ${sample_id}_1.fastq ]; then
+  rm ${sample_id}_1.fastq
+fi
+if [ -e ${sample_id}_2.fastq ]; then
+  rm ${sample_id}_2.fastq
+fi
 
+files1=`ls *_1.fastq | grep -v ${sample_id} | sort`
 for file in $files1; do
+   # Copy the contents of the run FASTQ into the sample FASTQ
    cat $file >> "${sample_id}_1.fastq"
+   
+   # Get the linked path for the original run FASTQ file and
+   # cleanup the run files.
    lpath=`stat -c %N  $file | awk -F"'" '{print $4}'`
    echo "clean_work_files.sh $lpath"
    if [ $publish == "null" ]; then
@@ -17,9 +28,13 @@ for file in $files1; do
    fi
 done
 
-files2=`ls *_2.fastq | sort`
+files2=`ls *_2.fastq | grep -v ${sample_id} | sort`
 for file in $files2; do
+   # Copy the contents of the run FASTQ into the sample FASTQ
    cat $file >> "${sample_id}_2.fastq"
+   
+   # Get the linked path for the original run FASTQ file and
+   # cleanup the run files.
    lpath=`stat -c %N  $file | awk -F"'" '{print $4}'`
    if [ $publish == "null" ]; then
       clean_work_files.sh $lpath
